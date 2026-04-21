64a155411a86fad82b2ca63b737ae1532d572087
mudi-merrey <akmerrey@gmail.com>
2026-04-21 11:20:48 +0800

    i added a comment for sufi

--- src/gittest/GitTest.java
+++ src/gittest/GitTest.java
@@ -14,7 +14,7 @@ public class GitTest {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        hello ahmed;
+        int sufi;
     }
     
 }
